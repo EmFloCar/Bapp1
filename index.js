d95132c67e166786a19f1f61d7ddabeaa13e864d
Emilio Flórez Carrascal <65036537+EmFloCar@users.noreply.github.com>
2022-10-20 05:04:23 -0500

    prueba 1

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const multer = require("multer");
 require('dotenv').config();
 mongoose.Promise = global.Promise;
 
-//create our app
 const app = express();
 
-//middleware
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan("tiny"));
@@ -20,22 +18,14 @@ app.use(express.json())
 
 const palabraPath = require("./routes/Palabras")
 const login = require("./routes/auth");
-//routes
+
 app.use("/", palabraPath)
 app.use("/", login)
 
-// //Settings
-// app.set("port", process.env.PORT || 4000);
-
-// //start server
-// app.listen(app.get("port"), () => {
-//   console.log("Server on port", app.get("port"));
-// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server on");
 })
-//DB conection
 
 mongoose
   .connect(process.env.DB, {
